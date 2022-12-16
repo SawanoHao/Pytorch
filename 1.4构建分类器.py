382c0505e1d2f249792bfeaffdfba96fad823d42
SawanoHao <474244189@qq.com>
2022-12-16 16:05:31 +0800

    update

--- 1.4构建分类器.py
+++ 1.4构建分类器.py
@@ -21,11 +21,13 @@ def imshow(img):
     # plt.show()
 
 
-dataiter = iter(trainloader)
-images, labels = next(dataiter)
-imshow(torchvision.utils.make_grid(images))
-# print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+# 展示训练集的图片
+# dataiter = iter(trainloader)
+# images, labels = next(dataiter)
+# imshow(torchvision.utils.make_grid(images))
+# print('TrainData:',' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
+# 构建神经网络
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -52,9 +54,53 @@ class Net(nn.Module):
 
 
 net = Net()
-print(net)
+# print(net)
 
+# 梯度下降找到最小损失函数的值
 import torch.optim as optim
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+# criterion = nn.CrossEntropyLoss()
+# optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+# for epoch in range(2):
+#     running_loss = 0.0
+#     for i, data in enumerate(trainloader, 0):
+#         inputs, labels = data
+#         optimizer.zero_grad()
+#         outputs = net(inputs)
+#         loss = criterion(outputs, labels)
+#         loss.backward()
+#         optimizer.step()
+#
+#         running_loss += loss.item()
+#         if (i + 1) % 2000 == 0:
+#             print('[%d,%5d] loss:%.3f' %
+#                   (epoch + 1, i + 1, running_loss / 2000))
+#             running_loss = 0.0
+# print('Finished Training')
+
+# 保存模型
+PATH = './cifar_net.pth'
+# torch.save(net.state_dict(), PATH)
+
+# 展示测试集的图片
+dataiter = iter(testloader)
+images, labels = next(dataiter)
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruth:', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+# 加载模型并对测试图片进行预测
+net.load_state_dict(torch.load(PATH))
+outputs = net(images)
+_, predicted = torch.max(outputs, 1)
+print('Predicted:', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+print('Accuracy of the network on the 10000 test  images: %d %%' % (100 * correct / total))
